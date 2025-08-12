dfbf9a95192ae1729d71b94922019c738652382d
SnehalButani <developer.snehalbutani@gmail.com>
2025-08-12 17:56:30 +0530

    setup the mongoose config

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_PUBLIC_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
